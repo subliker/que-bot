821b3bc760851b95cbdff6b992a4cb7ac9394d63
subliker <rapata321@gmail.com>
2025-03-31 11:40:41 +0300

    upd: handle button data length error

--- internal/bot/telebot/handlers_callback.go
+++ internal/bot/telebot/handlers_callback.go
@@ -43,7 +43,16 @@ func (c *controller) handleQueueQueryBtnNew() tele.HandlerFunc {
 		btn.Text = callbackBundle.Btns().SubmitFirst()
 		btn.Data = strings.Join([]string{string(queueID), queueName}, "|")
 		mk.Inline(tele.Row{btn})
-		ctx.Edit(callbackBundle.QueueNew().Main(queueName), mk)
+		if err := ctx.Edit(callbackBundle.QueueNew().Main(queueName), mk); err != nil {
+			if !strings.Contains(err.Error(), "True") {
+				if strings.Contains(err.Error(), "BUTTON_DATA_INVALID") {
+					errorsBundle := c.langBundle(ctx.Callback().Sender.LanguageCode).Errors()
+					ctx.Edit(errorsBundle.ButtonDataLength() +
+						errorsBundle.Tail())
+				}
+				return err
+			}
+		}
 		return nil
 	}
 }
